fe7fe1182c118fca2ff3ae95b2d2cd27b0a292db
ROBHACKZ1257 <rob.promail47@gmail.com>
2023-09-10 20:01:12 -0600

    Updating Portfolio

--- script.js
+++ script.js
@@ -1,25 +1,2 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const imageContainer = document.querySelector(".image-container-1");
-    const images = imageContainer.querySelectorAll("img");
-    let currentImageIndex = 0;
-  
-    // Initially, hide all images except the first one
-    images.forEach((image, index) => {
-      if (index !== currentImageIndex) {
-        image.style.display = "none";
-      }
-    });
-  
-    // Add a click event listener to the container
-    imageContainer.addEventListener("click", function () {
-      // Hide the current image
-      images[currentImageIndex].style.display = "none";
-  
-      // Increment the image index or reset it to 0 if it exceeds the number of images
-      currentImageIndex = (currentImageIndex + 1) % images.length;
-  
-      // Show the new current image
-      images[currentImageIndex].style.display = "block";
-    });
-  });
+
   
