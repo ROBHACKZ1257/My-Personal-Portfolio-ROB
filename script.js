953fda8ee0bca133c21b33226681dcb97b46c49b
ROBHACKZ1257 <rob.promail47@gmail.com>
2023-09-14 21:49:57 -0600

    Updating my Portfolio

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const clickElement = document.getElementById("click");
-  
-    clickElement.addEventListener("click", toggleLinks);
-  });
-  
-  function toggleLinks() {
-    const linksElement = document.getElementById("myLinks");
-    linksElement.style.display = (linksElement.style.display === "block") ? "none" : "block";
-  }
-  
+  const clickElement = document.getElementById("click");
+
+  clickElement.addEventListener("click", toggleLinks);
+});
+
+function toggleLinks() {
+  const linksElement = document.getElementById("myLinks");
+  linksElement.style.display = (linksElement.style.display === "block") ? "none" : "block";
+}
